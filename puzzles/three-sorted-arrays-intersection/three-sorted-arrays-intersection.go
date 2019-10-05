00d35879638169496ab3419b52b7c69658dc577c
Andrew Jones <andrew@bidops.com>
2019-10-05 13:41:48 -0400

    play around with incrementing via a pointer

--- puzzles/three-sorted-arrays-intersection/three-sorted-arrays-intersection.go
+++ puzzles/three-sorted-arrays-intersection/three-sorted-arrays-intersection.go
@@ -2,12 +2,19 @@ package main
 
 import "fmt"
 
-// actually not needed
+// actually not needed, just expiramenting
 func safeIncrement(index int, array []int) int {
 	if index < len(array)-1 {
 		return index + 1
 	}
 	return index
+}
+
+// not needing, expirementing
+func safeIncrementPointer(index *int, array []int) {
+	if *index < len(array) {
+		*index = *index + 1
+	}
 }
 
 func arraysIntersection(arr1 []int, arr2 []int, arr3 []int) []int {
@@ -43,4 +50,15 @@ func main() {
 	r := arraysIntersection(arr1, arr2, arr3)
 
 	fmt.Printf("%v", r)
+
+	var i int = 4
+	var arr []int = make([]int, 8)
+	fmt.Println(i)
+	safeIncrementPointer(&i, arr)
+	fmt.Println(i)
+
+	for i < len(arr) {
+		safeIncrementPointer(&i, arr)
+	}
+	fmt.Println(i)
 }
