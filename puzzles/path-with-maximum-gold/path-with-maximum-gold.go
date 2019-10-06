470afb969e2ec1715b4b8fbacf86ef55e7fbfa84
Andrew Jones <andrew@bidops.com>
2019-10-06 12:27:56 -0400

    function signatures

---
+++ puzzles/path-with-maximum-gold/path-with-maximum-gold.go
@@ -0,0 +1,20 @@
+package main
+
+func options(row int, column int, grid [][]int) []int {
+
+}
+
+func backtrack(row int, col int, currentGold *int, maxGold *int) {
+
+}
+
+func getMaximumGold(grid [][]int) int {
+
+	var currentGold int = 0
+	var maximumGold int = 0
+
+}
+
+func main() {
+
+}
