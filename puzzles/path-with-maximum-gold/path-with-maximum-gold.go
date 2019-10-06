72b5f30b356d77380e04e3cecf2a7cc3acf65f12
Andrew Jones <andrew@bidops.com>
2019-10-06 12:38:07 -0400

    basic algorithm structure

--- puzzles/path-with-maximum-gold/path-with-maximum-gold.go
+++ puzzles/path-with-maximum-gold/path-with-maximum-gold.go
@@ -1,18 +1,51 @@
 package main
 
-func options(row int, column int, grid [][]int) []int {
+type opt struct {
+	row int
+	col int
+}
+
+func options(row int, column int, grid [][]int) []opt {
 
 }
 
-func backtrack(row int, col int, currentGold *int, maxGold *int) {
+func backtrack(row int, col int, grid [][]int, currentGold *int, maxGold *int) {
+	var goldAtPos int = grid[row][col]
+
+	if goldAtPos == 0 {
+		return
+	}
+
+	*currentGold = *currentGold + goldAtPos
+
+	if *currentGold > *maxGold {
+		*maxGold = *currentGold
+	}
+
+	// clear out at this position to avoid duplicates
+	grid[row][col] = 0
+
+	for _, opt := range options(row, col, grid) {
+		backtrack(opt.row, opt.col, grid, currentGold, maxGold)
+	}
+
+	*currentGold = *currentGold - goldAtPos // revert
+	grid[row][col] = goldAtPos              // restore grid position as option for future backtracks
 
 }
 
 func getMaximumGold(grid [][]int) int {
 
 	var currentGold int = 0
-	var maximumGold int = 0
+	var maxGold int = 0
 
+	for row := range grid {
+		for column := range grid[row] {
+			backtrack(row, column, grid, &currentGold, &maxGold)
+		}
+	}
+
+	return maxGold
 }
 
 func main() {
