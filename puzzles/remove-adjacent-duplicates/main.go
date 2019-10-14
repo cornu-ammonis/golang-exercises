92f0d0ea3e17fff95a571e8b8fc1f8573048793d
Andrew Jones <andrew@bidops.com>
2019-10-14 12:01:03 -0400

    init singlePassRemove implementation and removeKFromStack signature

--- puzzles/remove-adjacent-duplicates/main.go
+++ puzzles/remove-adjacent-duplicates/main.go
@@ -6,8 +6,28 @@ import (
 	"../../ds/stack"
 )
 
-func singlePassRemove(s string, k int) string {
+func removeKFromStack(stk stack.ByteStack, k int) stack.ByteStack {
 
+}
+
+func singlePassRemove(s string, k int) string {
+	prev := byte(0)
+	sameCount := 0
+	stk := make(stack.ByteStack, 0)
+	for i, _ := range s {
+		var c byte = s[i]
+		stk = stk.Push(c)
+
+		if c == prev {
+			sameCount = sameCount + 1
+			if sameCount == k {
+				stk = removeKFromStack(stk, k)
+			}
+		} else {
+			sameCount = 0
+			prev = c
+		}
+	}
 }
 
 func removeDuplicates(s string, k int) string {
