63b542aa9fc086d29c3adb0dec93f19c158fec4c
Andrew Jones <andrew@bidops.com>
2019-10-14 11:41:40 -0400

    test relative import from another directory

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"../ds/stack"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+
+	s := make(stack.ByteStack, 0)
+	s = s.Push(1)
+	fmt.Println(s)
+	s = s.Push(2)
+	s = s.Push(3)
+	fmt.Println(s)
+	s, p := s.Pop()
+	fmt.Println(p)
 }
