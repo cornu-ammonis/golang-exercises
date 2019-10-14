f34c13735aaf521dd9980a2023af069c26a5ffd0
Andrew Jones <andrew@bidops.com>
2019-10-14 12:41:53 -0400

    attempt at creating O(n) solution

--- puzzles/remove-adjacent-duplicates/main.go
+++ puzzles/remove-adjacent-duplicates/main.go
@@ -4,52 +4,52 @@ package main
 
 import (
 	"fmt"
-
-	"../../ds/stack"
 )
 
-func removeKFromStack(stk stack.ByteStack, k int) stack.ByteStack {
-	for i := 0; i < k; i = i + 1 {
-		stk, _ = stk.Pop()
-	}
-
-	return stk
-}
-
-func singlePassRemove(s string, k int) string {
-	prev := byte(0)
-	sameCount := 0 // starts at 0 because we have 0 same char in a row
-	stk := make(stack.ByteStack, 0)
-	for i, _ := range s {
-		var c byte = s[i]
-		stk = stk.Push(c)
-
-		if c == prev {
-			sameCount = sameCount + 1
-			if sameCount == k {
-				stk = removeKFromStack(stk, k)
-			}
-		} else {
-			sameCount = 1 // starts at one here because we have 1 same char in a row
-			prev = c
-		}
-	}
-
-	return stk.ToString()
+type ch struct {
+	char  byte
+	count int
 }
 
 func removeDuplicates(s string, k int) string {
-	var prevLen int = 0
-	var currentLen int = len(s)
+	stack := make([]ch, 0)
+	firstCh := ch{char: s[0], count: 1}
+	stack = append(stack, firstCh)
 
-	for prevLen != currentLen {
-		prevLen = currentLen
+	for i := 1; i < len(s); i++ {
+		c := s[i]
 
-		s = singlePassRemove(s, k)
-		currentLen = len(s)
+		var prev ch
+		if len(stack) > 0 {
+			prev = stack[len(stack)-1]
+		}
+
+		if (prev != ch{}) && (prev.char == c) {
+			stack = append(stack, ch{
+				char:  c,
+				count: 1 + prev.count,
+			})
+		} else {
+			stack = append(stack, ch{
+				char:  c,
+				count: 1,
+			})
+		}
+
+		if stack[len(stack)-1].count == k {
+			for i := 0; i < k; i++ {
+				stack = stack[:len(stack)-1]
+			}
+		}
+
 	}
 
-	return s
+	res := make([]byte, len(stack))
+
+	for i := 0; i < len(stack); i++ {
+		res = append(res, stack[i].char)
+	}
+	return string(res)
 }
 
 func main() {
