94ec6bff0c201779dde5d72296b4f3a71fa6a447
Andrew Jones <andrew@bidops.com>
2019-10-19 12:09:43 -0400

    meeting scheduler init

---
+++ puzzles/meeting-scheduler/meeting-scheduler.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//https://leetcode.com/problems/meeting-scheduler/
+func minAvailableDuration(slots1 [][]int, slots2 [][]int, duration int) []int {
+
+}
+
+func main() {
+	fmt.Println("hi")
+}
