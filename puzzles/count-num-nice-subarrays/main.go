170b3704ba8d0ff17c83b2c938923f49d0a2ffa5
Andrew Jones <andrew@bidops.com>
2019-11-06 14:23:29 -0500

    function signatures

---
+++ puzzles/count-num-nice-subarrays/main.go
@@ -0,0 +1,35 @@
+package main
+
+/* https://leetcode.com/problems/count-number-of-nice-subarrays/
+Given an array of integers nums and an integer k. A subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+
+*/
+
+// i is the index on oddIndices. padding left is the space between
+// the oddIndex at i and either the beginning of the nums array, or the next
+// odd number to the left
+func paddingLeft(oddIndices []int, i int) {
+
+}
+
+func isOdd(n int) int {
+
+}
+
+func numberOfSubarrays(nums []int, k int) int {
+
+}
+
+func main() {
+
+}
