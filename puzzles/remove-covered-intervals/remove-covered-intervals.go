ece278f92e8bbfdb30568e5070f418bffaa8418e
Andrew Jones <andrew@bidops.com>
2019-12-14 12:27:43 -0500

    init test

--- puzzles/remove-covered-intervals/remove-covered-intervals.go
+++ puzzles/remove-covered-intervals/remove-covered-intervals.go
@@ -1,9 +1,17 @@
 package main
 
+import "fmt"
+
 // https://leetcode.com/problems/remove-covered-intervals/
 // Given a list of intervals, remove all intervals that are covered by another interval in the list.
 // Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
 // After doing so, return the number of remaining intervals.
 func removeCoveredIntervals(intervals [][]int) int {
 
+}
+
+func main() {
+	var test1 [][]int = [][]int{{1, 4}, {3, 6}, {2, 8}}
+
+	fmt.Println(removeCoveredIntervals(test1)) // expects 2
 }
