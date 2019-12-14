230334280c5f1019c41984c6762993422b0c9da6
Andrew Jones <andrew@bidops.com>
2019-12-14 12:35:41 -0500

    create and test remove function

--- puzzles/remove-covered-intervals/remove-covered-intervals.go
+++ puzzles/remove-covered-intervals/remove-covered-intervals.go
@@ -5,6 +5,11 @@ import (
 	"sort"
 )
 
+// https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang
+func remove(slice [][]int, s int) [][]int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
 // https://leetcode.com/problems/remove-covered-intervals/
 // Given a list of intervals, remove all intervals that are covered by another interval in the list.
 // Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
@@ -14,6 +19,8 @@ func removeCoveredIntervals(intervals [][]int) int {
 	// sort the slice based on first element for now
 	sort.Slice(intervals, func(i, j int) bool { return intervals[i][0] < intervals[j][0] })
 
+	fmt.Println(intervals)
+	intervals = remove(intervals, 1)
 	fmt.Println(intervals)
 	return 0
 }
