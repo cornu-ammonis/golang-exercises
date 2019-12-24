13abcfd851b7276f3e194d5c46db5af1715a6d1e
Andrew Jones <andrew@bidops.com>
2019-12-24 13:40:03 -0500

    sequential digits scaffolding

---
+++ puzzles/sequential-digits/sequential-digits.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://leetcode.com/problems/sequential-digits/
+// An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+// Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+func sequentialDigits(low int, high int) []int {
+
+}
+
+func main() {
+	fmt.Println("hello")
+
+}
