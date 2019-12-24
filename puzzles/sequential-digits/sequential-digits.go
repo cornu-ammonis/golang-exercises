4b9c95edfc03eeaf53613274d9a10ee03fc744aa
Andrew Jones <andrew@bidops.com>
2019-12-24 13:42:26 -0500

    leftmostDigit function

--- puzzles/sequential-digits/sequential-digits.go
+++ puzzles/sequential-digits/sequential-digits.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 )
 
+func leftmostDigit(n int) int {
+	for n >= 10 {
+		n = n / 10
+	}
+
+	return n
+}
+
 // https://leetcode.com/problems/sequential-digits/
 // An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
 // Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
-
 func sequentialDigits(low int, high int) []int {
-
+	return []int{}
 }
 
 func main() {
 	fmt.Println("hello")
 
+	fmt.Println(leftmostDigit(123))
+	fmt.Println(leftmostDigit(456))
 }
