1a8f245339af361488cee291825bb8eb7efc668c
Andrew Jones <andrew@bidops.com>
2020-01-20 14:37:46 -0500

    makeDynamic2DArray implementation

--- puzzles/make-connected/make-connected.go
+++ puzzles/make-connected/make-connected.go
@@ -4,8 +4,24 @@ import (
 	"fmt"
 )
 
-func makeConnected(n int, connections [][]int) int {
+func makeDynamic2DArray(n int) [][]int {
+	matrix := make([][]int, n)
 
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+
+	return matrix
+}
+
+func buildAdjacencyMatrix(connections [][]int, n int) [][]int {
+	var matrix [][]int = makeDynamic2DArray(n)
+
+}
+
+// https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+func makeConnected(n int, connections [][]int) int {
+	return 0
 }
 
 func main() {
